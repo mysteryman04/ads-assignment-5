f516c49b8bfe36502df502f86cef5c5a95250dc1
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-21 21:35:44 +0600

    In put, if the current node is null, a new node is created with the given key and value, and it's returned. If the current node is not null, the key is compared with the current node's key. If the key is less than the current node's key, put is recursively called on the left subtree. If greater than the current node's key, put is recursively called on the right subtree. If the key is equal, the associated value is updated.

--- src/BST.java
+++ src/BST.java
@@ -9,10 +9,28 @@ public class BST<K extends Comparable<K>, V> {
             this.key = key;
             this.val = val;
         }
-        public void put(K key, V val) {}
+        public void put(K key, V val) {
+            root = put(root, key, val);
+        }
+        private Node put(Node node, K key, V val) {
+            if (node == null) {
+                return new Node(key, val);
+            }
+
+            int cmp = key.compareTo(node.key);
+            if (cmp < 0) {
+                node.left = put(node.left, key, val);
+            } else if (cmp > 0) {
+                node.right = put(node.right, key, val);
+            } else {
+                node.val = val;
+            }
+
+            return node;
+        }
 //        public V get (K key){}
 //        public void delete (K key){}
-//        public Iterable<K> iterator () {}
-        }
+//        public Iterable<K> iterator () {}/
     }
 }
+
